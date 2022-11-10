bc5409aaaf06e35e33c24c36b38aab708cda7f27
MESHACKKIBET <kibetkoros973@gmail.com>
2022-11-10 13:17:19 +0300

    commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hello world program";
+
+$myName = "Meshack Kibet";
+echo $myName;
+var_dump($myName);
+
+// intergers
+$a =30;
+$b ='30';
+$c = true;
+$d = 200.34;
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+
+echo ".My Name is".$myName. ". My MPESA balance is Ksh" .$d;
+
+?>
